c1f5736c523f73e7aac1c6efce34d81eb036daa7
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-04 23:57:21 +0200

    add toaster provider

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,9 +4,11 @@ import { trpc } from "@/app/_trpc/client";
 import { absoluteUrl } from "@/lib/utils";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
+import { Toaster } from "react-hot-toast";
 
 const Providers = ({ children }: PropsWithChildren) => {
+  const [isMounted, setIsMounted] = useState(false);
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
@@ -18,9 +20,18 @@ const Providers = ({ children }: PropsWithChildren) => {
     }),
   );
 
+  useEffect(() => {
+    if (!isMounted) {
+      setIsMounted(true);
+    }
+  }, [isMounted]);
+
+  if (!isMounted) return null;
+
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <Toaster />
     </trpc.Provider>
   );
 };
