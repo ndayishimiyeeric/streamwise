139346d5dc69a9a3d5bc2b035ae6669b25d4213a
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-07 22:41:21 +0200

    add a get file schema

--- src/lib/validators/file.ts
+++ src/lib/validators/file.ts
@@ -5,3 +5,9 @@ export const DeleteFileInput = z.object({
 });
 
 export type DeleteFileInputType = z.infer<typeof DeleteFileInput>;
+
+export const GetFileInput = z.object({
+  key: z.string(),
+});
+
+export type GetFileInputType = z.infer<typeof GetFileInput>;
