96eb3e12563bfad8c2af55f912094b30aa3c5d0f
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-07 22:40:52 +0200

    add dropzone uploader

---
+++ src/components/upload-dropzone.tsx
@@ -0,0 +1,129 @@
+"use client";
+
+import React, { useState } from "react";
+import Dropzone from "react-dropzone";
+import { Cloud, File, Loader2 } from "lucide-react";
+import { Progress } from "@/components/ui/progress";
+import { useUploadThing } from "@/lib/uploadthing";
+import toast from "react-hot-toast";
+import { trpc } from "@/app/_trpc/client";
+import { useRouter } from "next/navigation";
+
+function UploadDropzone() {
+  const router = useRouter();
+  const [isUploading, setIsUploading] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+
+  const { startUpload } = useUploadThing("pdfUploader");
+  const { mutate: startPolling } = trpc.getFile.useMutation({
+    onSuccess: (data) => {
+      router.push(`/dashboard/${data.id}`);
+    },
+    retry: 3,
+    retryDelay: 500,
+  });
+
+  const startProgressSimulation = () => {
+    setUploadProgress(0);
+
+    const interval = setInterval(() => {
+      setUploadProgress((prev) => {
+        if (prev >= 95) {
+          clearInterval(interval);
+          return prev;
+        }
+        return prev + 5;
+      });
+    }, 500);
+
+    return interval;
+  };
+
+  return (
+    <Dropzone
+      multiple={false}
+      onDrop={async (acceptedFile) => {
+        setIsUploading(true);
+        const interval = startProgressSimulation();
+
+        // handle file upload
+        const res = await startUpload(acceptedFile);
+        if (!res) {
+          return toast.error("Something went wrong. Please try again later.");
+        }
+
+        const [fileResponse] = res;
+        const key = fileResponse.key;
+
+        if (!key) {
+          return toast.error("Something went wrong. Please try again later.");
+        }
+
+        clearInterval(interval);
+        setUploadProgress(100);
+
+        startPolling({ key });
+      }}
+    >
+      {({ getRootProps, getInputProps, acceptedFiles }) => (
+        <div
+          {...getRootProps()}
+          className="border border-dashed border-gray-300 h-64 m-4 rounded-lg"
+        >
+          <div className="flex items-center justify-center h-full w-full">
+            <label
+              htmlFor="dropzone-file"
+              className="bg-gray-50 flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer hover:bg-gray-100"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                <p className="text-sm text-zinc-700 mb-2 text-center">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-zinc-500">PDF (up to 4MB)</p>
+              </div>
+
+              {acceptedFiles && acceptedFiles[0] && (
+                <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                  <div className="px-3 py-2 h-full grid place-items-center">
+                    <File className="h-4 w-4 text-blue-500" />
+                  </div>
+                  <div className="px-3 py-2 h-full text-sm truncate">
+                    {acceptedFiles[0].name}
+                  </div>
+                </div>
+              )}
+
+              {isUploading && (
+                <div className="w-full mt-4 max-w-xs mx-auto">
+                  <Progress
+                    value={uploadProgress}
+                    className="h-1 w-full bg-zinc-100"
+                    customIndicator={
+                      uploadProgress === 100 ? "bg-green-500" : ""
+                    }
+                  />
+                  {uploadProgress === 100 && (
+                    <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2">
+                      <Loader2 className="h-3 w-3 animate-spin" />
+                      <span>Processing...</span>
+                    </div>
+                  )}
+                </div>
+              )}
+              <input
+                {...getInputProps()}
+                type="file"
+                id="dropzone-file"
+                className="hidden"
+              />
+            </label>
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  );
+}
+
+export default UploadDropzone;
