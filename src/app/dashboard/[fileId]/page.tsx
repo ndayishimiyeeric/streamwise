7c2deac7c3e5a8a62ff9352b13b9b1a00fe99e02
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-08 21:11:13 +0200

    render pdf

---
+++ src/app/dashboard/[fileId]/page.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import PdfRenderer from "@/components/pdf-renderer";
+import ChatWrapper from "@/components/chat-wrapper";
+
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+type Props = {
+  params: {
+    fileId: string;
+  };
+};
+
+async function Page({ params }: Props) {
+  const { fileId } = params;
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
+  if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileId}`);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId: user.id,
+    },
+  });
+
+  if (!file) notFound();
+
+  return (
+    <div className="flex flex-1 flex-col justify-between h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        {/*PDF Side*/}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            <PdfRenderer url={file.url} />
+          </div>
+        </div>
+
+        {/*Chat Side*/}
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page;
