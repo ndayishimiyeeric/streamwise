2e54528b12858315fb1671c1adaafc57cedb75df
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-11 22:58:58 +0200

    create a vectors with a textKey

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,11 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
 import { db } from "@/lib/db";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { getPinecone } from "@/lib/pinecone";
 
 const f = createUploadthing();
 
@@ -28,6 +33,52 @@ export const ourFileRouter = {
           uploadStatus: "PROCESSING",
         },
       });
+
+      try {
+        const response = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+        );
+        const blob = await response.blob();
+        const loader = new PDFLoader(blob);
+
+        const pageLevelDocs = await loader.load();
+        const pages = pageLevelDocs.length;
+
+        const { index } = await getPinecone();
+
+        // indexing and vectorised documents
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY!,
+        });
+
+        const vectorizedDoc = await PineconeStore.fromDocuments(
+          pageLevelDocs,
+          embeddings,
+          {
+            pineconeIndex: index,
+            textKey: createdFile.id,
+          },
+        );
+
+        await db.file.update({
+          where: {
+            id: createdFile.id,
+          },
+          data: {
+            uploadStatus: "COMPLETED",
+          },
+        });
+      } catch (e) {
+        console.log(e);
+        await db.file.update({
+          where: {
+            id: createdFile.id,
+          },
+          data: {
+            uploadStatus: "FAILED",
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
 
