3049ff2ca76cbd4f476b327aa2b1068076f3272c
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-12 22:08:39 +0200

    add a get message zod schema

--- src/lib/validators/message.ts
+++ src/lib/validators/message.ts
@@ -4,3 +4,9 @@ export const SendMessageSchema = z.object({
   fileId: z.string().uuid(),
   message: z.string(),
 });
+
+export const GetFileMessagesSchema = z.object({
+  limit: z.number().min(1).max(100).nullish(),
+  cursor: z.string().nullish(),
+  fileId: z.string().uuid(),
+});
