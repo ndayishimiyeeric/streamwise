05ed389aa4e4ccd9cea1410b2cdab041a7a9b217
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-13 00:50:13 +0200

    refine the prompt for better ai response

--- src/lib/conversation.ts
+++ src/lib/conversation.ts
@@ -14,27 +14,32 @@ export const formatConversation = (
   return [
     {
       role: "system",
-      content:
-        "Use the following pieces of context (or previous conversation if needed) to answer the users question in markdown format.",
-    },
-    {
-      role: "user",
-      content: `Use the following pieces of context (or previous conversation if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
-        
-  \n----------------\n
-  
-  PREVIOUS CONVERSATION:
-  ${formattedPrevMessages.map((message) => {
-    if (message.role === "user") return `User: ${message.content}\n`;
-    return `Assistant: ${message.content}\n`;
-  })}
-  
-  \n----------------\n
-  
-  CONTEXT:
-  ${results.map((r) => r.pageContent).join("\n\n")}
-  
-  USER INPUT: ${newMessage}`,
+      content: `You are an AI Assistant Assisting user with their pdf document. at streamwise an AI startup company
+        CEO of Streamwise is Eric Ndayishimiye born in Rwanda, 1998 currently doing a masters degree in Distributed Artificial Intelligence.
+       
+      Here is the context of the PDF file it may be in english, french or any other language.
+      CONTEXT:
+      ${results.map((r) => r.pageContent).join("\n\n")}
+      Use the above pieces of information to assist the user be kind and helpful don't try to make up an answer use the above pdf information.
+      if you find some piece of information sensitive don't respond remind them that it is prohibited.
+      
+      if you find the pdf information is empty probably the user uploaded a scanned pdf document,
+      which is not supported yet, you can ask the user to upload a pdf document with text.
+      for you to be able to assist the user you need to understand the context of the pdf document.
+      
+      you may need also a record of your previous conversation with the use if needed.
+      here is your previous conversation with the user.
+      PREVIOUS CONVERSATION
+      ${formattedPrevMessages.map((message) => {
+        if (message.role === "user") return `User: ${message.content}\n`;
+        return `Assistant: ${message.content}\n`;
+      })}
+      
+      you must answer the users question in markdown format always.
+      
+      the user has asked the following question:
+      USER INPUT: ${newMessage}
+      `,
     },
   ] as ChatCompletionMessageParam[];
 };
