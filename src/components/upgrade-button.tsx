a99364cb524c0e084afdba1031a3cca2060cbf51
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-14 05:20:51 +0200

    add stripe session

--- src/components/upgrade-button.tsx
+++ src/components/upgrade-button.tsx
@@ -1,15 +1,24 @@
+"use client";
+
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { ArrowRight } from "lucide-react";
+import { trpc } from "@/app/_trpc/client";
 
 type Props = {
   plan: "Free" | "Silver" | "Gold";
 };
 
 function UpgradeButton({ plan }: Props) {
+  const { mutate: createStripeSession } = trpc.createStripeSession.useMutation({
+    onSuccess: ({ url }) => {
+      window.location.href = url ?? "/dashboard/billing";
+    },
+  });
   return (
     <Button
+      onClick={() => createStripeSession({ plan: plan })}
       className={cn("w-full text-black font-semibold", {
         "gradient-gold": plan === "Gold",
         "gradient-silver": plan === "Silver",
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function absoluteUrl(path: string) {
   if (typeof window !== "undefined") return path;
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
+  if (process.env.NEXT_BASE_URL)
+    return `https://${process.env.NEXT_BASE_URL}${path}`;
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
