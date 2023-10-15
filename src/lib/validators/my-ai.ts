7199d57558d2a3f1ec875dabe1901069e0239ac8
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-15 18:33:06 +0200

    add aidata validator

---
+++ src/lib/validators/my-ai.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const MyAiDataSchema = z.object({
+  name: z.string().min(2).max(50),
+  bio: z.string().min(10).max(200),
+  imgUrl: z.string().url(),
+});
+
+export type MyAiBioType = z.infer<typeof MyAiDataSchema>;
