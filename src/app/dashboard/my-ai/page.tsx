8903463d74d1e74c6175bac7e940caf426e4eda8
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-15 23:59:45 +0200

    render my ai page

---
+++ src/app/dashboard/my-ai/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+import { db } from "@/lib/db";
+import getSubscription from "@/lib/actions";
+import MyAiForm from "@/app/dashboard/my-ai/_components/my-ai-form";
+import { redirect } from "next/navigation";
+
+async function Page() {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+  const userId = user?.id;
+  if (!userId) {
+    redirect("/auth-callback?origin=/dashboard/my-ai");
+  }
+  const aiData = await db.aiData.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  if (!aiData) {
+    redirect("/dashboard");
+  }
+
+  const subscriptionPlan = await getSubscription();
+  return <MyAiForm subscriptionPlan={subscriptionPlan} aiData={aiData} />;
+}
+
+export default Page;
