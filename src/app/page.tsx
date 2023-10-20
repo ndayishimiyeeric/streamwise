fca2c1feffaef79e0959fbec4898506e48c7963f
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-21 01:32:27 +0200

    fix styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,12 +53,12 @@ export default function Home() {
               <div className="mt-16 flow-root sm:mt-24">
                 <div className="-m-2 rounded-2xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                   <Image
-                    src="/dashboard-preview.jpg"
+                    src="/dashboard-preview.png"
                     alt="product preview"
-                    width={1364}
-                    height={866}
+                    width={1907}
+                    height={961}
                     quality={100}
-                    className="pointer-events-none user-select-none rounded-md bg-white p-2 sm:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                    className="object-contain pointer-events-none user-select-none rounded-md bg-white p-2 sm:p-20 shadow-2xl ring-1 ring-gray-900/10"
                   />
                 </div>
               </div>
