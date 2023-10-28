d78ebb539b34f58889c192b471c30db465cafc6b
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-29 00:18:32 +0200

    add rechart graph

---
+++ src/components/overview.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React from "react";
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import { GraphData } from "@/lib/actions/user-usage-actions";
+
+type Props = {
+  data: GraphData[];
+};
+
+function Overview({ data }: Props) {
+  return (
+    <ResponsiveContainer width="90%" height={350}>
+      <BarChart data={data}>
+        <XAxis
+          dataKey="name"
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />
+
+        <YAxis
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) => `${value}`}
+        />
+        <Bar dataKey="value" fill="#80B3FF" radius={[4, 4, 0, 0]} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
+
+export default Overview;
