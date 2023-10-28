c3b3f549269d73572f50efd356cc4e438a2fe30c
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-28 12:49:27 +0200

    add usage progress bar

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -4,17 +4,32 @@ import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
 
-type ProgressProps = React.ComponentPropsWithoutRef<
-  typeof ProgressPrimitive.Root
-> & {
-  customIndicator?: string;
-};
+const progressVariants = cva("h-full w-full flex-1 bg-primary transition-all", {
+  variants: {
+    variant: {
+      default: "bg-sky-600",
+      success: "bg-emerald-700",
+      danger: "bg-red-700",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
+
+export interface ProgressProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof progressVariants> {}
+
+type CombinedProgressProps = ProgressProps &
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>;
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  ProgressProps
->(({ className, value, customIndicator, ...props }, ref) => (
+  CombinedProgressProps
+>(({ className, value, variant, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -24,10 +39,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className={cn(
-        "h-full w-full flex-1 bg-primary transition-all",
-        customIndicator ?? `${customIndicator}`,
-      )}
+      className={cn(progressVariants({ variant }))}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
