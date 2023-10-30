a6cbffd9edb6ac4acc985c57f4445d76f9f81f4e
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-10-30 20:35:41 +0100

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
 
 export default authMiddleware;
