2ec7b224eaa178faef6d84e547e606e7880fcab4
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2024-01-03 12:06:56 +0100

    add appearance action

---
+++ src/actions/appearance.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { auth } from "@/auth";
+import { getUserById, getUserSettingsById } from "@/data/user";
+import { AppearanceSchema } from "@/schemas";
+import * as z from "zod";
+
+import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
+
+export const appearance = async (input: z.infer<typeof AppearanceSchema>) => {
+  const session_user = await auth();
+  if (!session_user) return { error: "Unauthorized" };
+  const user = await getUserById(session_user.user.id);
+  if (!user) return { error: "Unauthorized" };
+  const user_settings = await getUserSettingsById(user.id);
+  if (!user_settings) return { error: "Unauthorized" };
+
+  await db.userSettings.update({
+    where: { id: user_settings.id },
+    data: { ...input },
+  });
+
+  return { success: "Settings updated" };
+};
