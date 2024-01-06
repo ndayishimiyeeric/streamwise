6be0642c0f16bf5ce95cb5efce1ff8c613809a9f
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2024-01-06 07:46:05 +0100

    add navbar and footer to landing page

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,6 +1,24 @@
+"use client";
+
 import React, { PropsWithChildren } from "react";
 
+import { PublicNavbar } from "./_components/navbar";
+
+import "./marketing.css";
+
+import { Footer } from "./_components/footer";
+import { Lines } from "./_components/lines";
+import ScrollToTop from "./_components/scrollToTop";
+
 const MarketingLayout = ({ children }: PropsWithChildren) => {
-  return <>{children}</>;
+  return (
+    <main className="relative min-h-screen">
+      <Lines />
+      <PublicNavbar />
+      {children}
+      <Footer />
+      <ScrollToTop />
+    </main>
+  );
 };
 export default MarketingLayout;
