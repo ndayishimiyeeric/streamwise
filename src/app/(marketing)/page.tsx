25494a9a2b3e33f24163c7ce8b0e4a42cdcd1fde
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2024-01-06 07:45:19 +0100

    structure landing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,148 +1,32 @@
-import Image from "next/image";
-import Link from "next/link";
-import { ArrowRight } from "lucide-react";
-
 import { cn } from "@/lib/utils";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { LoginButton } from "@/components/auth/login-button";
-import MaxWidthWrapper from "@/components/max-width-wrapper";
+
+import { About } from "./_components/about";
+import { Contact } from "./_components/contact";
+import { CTA } from "./_components/cta";
+import { Feature } from "./_components/features";
+import Hero from "./_components/hero";
+import { Pricing } from "./_components/pricing";
+import SectionWrapper from "./_components/section-wrapper";
+import { styles } from "./_components/styles";
+
+const FeaturesSection = SectionWrapper({ Component: Feature, idName: "features" });
+const PricingSection = SectionWrapper({ Component: Pricing, idName: "pricing" });
+const HeroSection = SectionWrapper({ Component: Hero, idName: "home" });
+const ContactSection = SectionWrapper({ Component: Contact, idName: "contact" });
 
 export default function Home() {
   return (
-    <>
-      <LoginButton mode="modal" asChild>
-        <Button>Login</Button>
-      </LoginButton>
-      <MaxWidthWrapper className="mb-12 mt-28 flex flex-col items-center justify-center text-center sm:mt-40">
-        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/30">
-          <p className="text-sm font-semibold text-gray-700">Streamwise is public!</p>
-        </div>
-
-        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-          Chat with your <span className="text-blue-600">documents</span> in seconds
-        </h1>
-
-        <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-          Streamwise allows you to have conversations with any PDF document. Simply upload your file
-          and start asking questions right away.
-        </p>
-
-        <Link className={cn(buttonVariants({ size: "lg" }), "mt-5")} href="/dashboard">
-          Dashboard <ArrowRight className="ml-2 h-5 w-5" />
-        </Link>
-      </MaxWidthWrapper>
-
-      <div>
-        <div className="relative isolate">
-          <div
-            aria-hidden="true"
-            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          >
-            <div
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]  opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            />
-          </div>
-
-          <div>
-            <div className="mx-auto max-w-6xl px-6 lg:px-8">
-              <div className="mt-16 flow-root sm:mt-24">
-                <div className="-m-2 rounded-2xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-                  <Image
-                    src="/dashboard-preview.png"
-                    alt="product preview"
-                    width={1907}
-                    height={961}
-                    quality={100}
-                    className="user-select-none pointer-events-none rounded-md bg-white object-contain p-2 shadow-2xl ring-1 ring-gray-900/10 sm:p-20"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div
-            aria-hidden="true"
-            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          >
-            <div
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-              className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]  opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"
-            />
-          </div>
+    <main className="min-h-screen overflow-hidden pb-30 pt-20">
+      <HeroSection />
+      <div className={cn(styles.paddingX, styles.flexStart, "")}>
+        <div className={cn(styles.boxWidth, "")}>
+          <FeaturesSection />
+          <About />
+          <CTA />
+          <PricingSection />
+          <ContactSection />
         </div>
       </div>
-
-      {/*Feature section*/}
-      <div className="mx-auto my-32 max-w-5xl sm:mt-56">
-        <div className="mb-12 px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl sm:text-center">
-            <h2 className="mt-2 text-4xl font-bold text-gray-900 sm:text-5xl">
-              Start chatting in minutes
-            </h2>
-            <p className="mt-4 text-lg text-gray-600">
-              Chatting to your PDF documents has never been easier. Simply streamwise your PDF and
-              start chatting.
-            </p>
-          </div>
-        </div>
-
-        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
-          <li className="md:flex-1">
-            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
-              <span className="text-sm font-medium text-blue-600">Step 1</span>
-              <span className="text-xl font-semibold">Sign up for an account</span>
-              <span className="mt-2 text-zinc-700">
-                Either starting with a free plan or choose a{" "}
-                <Link href="/pricing" className="text-blue-700 underline underline-offset-2">
-                  pro plan.
-                </Link>
-              </span>
-            </div>
-          </li>
-
-          <li className="md:flex-1">
-            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
-              <span className="text-sm font-medium text-blue-600">Step 2</span>
-              <span className="text-xl font-semibold">Upload your PDF document</span>
-              <span className="mt-2 text-zinc-700">
-                We process your file and make it ready for you to chat with.
-              </span>
-            </div>
-          </li>
-
-          <li className="md:flex-1">
-            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
-              <span className="text-sm font-medium text-blue-600">Step 3</span>
-              <span className="text-xl font-semibold">Start chatting</span>
-              <span className="mt-2 text-zinc-700">
-                It&apos;s that simple. Start chatting in less than a minute.
-              </span>
-            </div>
-          </li>
-        </ol>
-
-        <div className="mx-auto max-w-6xl px-6 lg:px-8">
-          <div className="mt-16 flow-root sm:mt-24">
-            <div className="-m-2 rounded-2xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-              <Image
-                src="/file-upload-preview.jpg"
-                alt="product preview"
-                width={1419}
-                height={732}
-                quality={100}
-                className="user-select-none pointer-events-none rounded-md bg-white p-2 shadow-2xl ring-1 ring-gray-900/10 sm:p-20"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    </main>
   );
 }
