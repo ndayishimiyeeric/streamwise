8cea971b84fab1e9e74cd839912f125c423eafb9
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2024-01-08 13:39:02 +0100

    add a custom zod price type for price validation

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,9 @@
-import { Product, Subscription } from "@prisma/client";
+import { Price, Product, Subscription } from "@prisma/client";
 
 export interface SubscriptionWithProduct extends Subscription {
   product: Product;
+}
+
+export interface PriceWithQuantity extends Price {
+  quantity?: number;
 }
