d8115caf46f0046ee9c8155e282ba39af6ccdb6a
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2024-01-08 22:57:46 +0100

    add pricing table

--- src/app/(marketing)/_components/pricing/index.tsx
+++ src/app/(marketing)/_components/pricing/index.tsx
@@ -1,18 +1,18 @@
-"use client";
-
 import Image from "next/image";
-import { ArrowRight } from "lucide-react";
+import { getActiveProductsWithPrices } from "@/data/subscriptions";
 import { FaArrowRight } from "react-icons/fa";
+
+import { Pricing as PricingTable } from "@/components/billing/pricing";
 
 import { SectionHeader } from "../section-header";
 
-export const Pricing = () => {
+export const Pricing = async () => {
+  const products = await getActiveProductsWithPrices();
+
   return (
     <>
-      {/* <!-- ===== Pricing Table Start ===== --> */}
       <section className="container overflow-hidden pb-20 pt-15 lg:pb-25 xl:pb-30">
         <div className="mx-auto max-w-c-1315 px-4 md:px-8 xl:px-0">
-          {/* <!-- Section Title Start --> */}
           <div className="animate_top mx-auto text-center">
             <SectionHeader
               headerInfo={{
@@ -22,7 +22,6 @@ export const Pricing = () => {
               }}
             />
           </div>
-          {/* <!-- Section Title End --> */}
         </div>
 
         <div className="relative mx-auto mt-15 max-w-[1207px] px-4 md:px-8 xl:mt-20 xl:px-0">
@@ -34,110 +33,10 @@ export const Pricing = () => {
               className="dark:hidden"
             />
           </div>
-          <div className="grid grid-cols-1 place-items-center gap-7.5 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3  xl:gap-12.5">
-            {/* <!-- Pricing Items --> */}
 
-            <div className="animate_top group relative rounded-lg border border-stroke bg-white p-7.5 shadow-solid-10 dark:border-muted dark:bg-card dark:shadow-none xl:p-12.5">
-              <h3 className="mb-7.5 text-3xl font-bold text-primary xl:text-sectiontitle3">
-                $59{" "}
-                <span className="text-regular text-muted-foreground dark:text-manatee">/month</span>
-              </h3>
-              <h4 className="mb-2.5 text-para2 font-medium text-black dark:text-white">
-                Medium Pack
-              </h4>
-              <p>Lorem ipsum dolor sit amet, consec adipisicing elit.</p>
-
-              <div className="mt-9 border-t border-stroke pb-12.5 pt-9 dark:border-strokedark">
-                <ul>
-                  <li className="mb-4 text-black last:mb-0 dark:text-manatee">300 GB Storage</li>
-                  <li className="mb-4 text-black last:mb-0 dark:text-manatee">
-                    Unlimited Photos and Videos
-                  </li>
-                  <li className="mb-4 text-black last:mb-0 dark:text-manatee">Exclusive Support</li>
-                  <li className="mb-4 text-black opacity-40 last:mb-0 dark:text-manatee">
-                    Custom Branding Strategy
-                  </li>
-                </ul>
-              </div>
-
-              <button
-                aria-label="Get the Plan button"
-                className="group/btn inline-flex items-center gap-2.5 font-medium text-primary transition-all duration-300 dark:text-white dark:hover:text-primary"
-              >
-                <span className="duration-300 group-hover/btn:pr-2">Get the Plan</span>
-                <FaArrowRight className="h-4 w-4" />
-              </button>
-            </div>
-            <div className="animate_top group relative rounded-lg border border-stroke bg-white p-7.5 shadow-solid-10 dark:border-muted dark:bg-card dark:shadow-none xl:p-12.5">
-              <div className="absolute -right-3.5 top-12.5 -rotate-90 rounded-bl-full rounded-tl-full bg-primary px-4.5 py-1.5 text-metatitle font-medium uppercase text-white dark:text-black">
-                Recommended
-              </div>
-
-              <h3 className="mb-7.5 text-3xl font-bold text-primary xl:text-sectiontitle3">
-                $59{" "}
-                <span className="text-regular text-muted-foreground dark:text-manatee">/month</span>
-              </h3>
-              <h4 className="mb-2.5 text-para2 font-medium text-black dark:text-white">
-                Medium Pack
-              </h4>
-              <p>Lorem ipsum dolor sit amet, consec adipisicing elit.</p>
-
-              <div className="mt-9 border-t border-stroke pb-12.5 pt-9 dark:border-strokedark">
-                <ul>
-                  <li className="mb-4 text-black last:mb-0 dark:text-manatee">300 GB Storage</li>
-                  <li className="mb-4 text-black last:mb-0 dark:text-manatee">
-                    Unlimited Photos and Videos
-                  </li>
-                  <li className="mb-4 text-black last:mb-0 dark:text-manatee">Exclusive Support</li>
-                  <li className="mb-4 text-black opacity-40 last:mb-0 dark:text-manatee">
-                    Custom Branding Strategy
-                  </li>
-                </ul>
-              </div>
-
-              <button
-                aria-label="Get the Plan button"
-                className="group/btn inline-flex items-center gap-2.5 font-medium text-primary transition-all duration-300 dark:text-white dark:hover:text-primary"
-              >
-                <span className="duration-300 group-hover/btn:pr-2">Get the Plan</span>
-                <FaArrowRight className="h-4 w-4" />
-              </button>
-            </div>
-            <div className="animate_top group relative rounded-lg border border-stroke bg-white p-7.5 shadow-solid-10 dark:border-muted dark:bg-card dark:shadow-none xl:p-12.5">
-              <h3 className="mb-7.5 text-3xl font-bold text-primary xl:text-sectiontitle3">
-                $59{" "}
-                <span className="text-regular text-muted-foreground dark:text-manatee">/month</span>
-              </h3>
-              <h4 className="mb-2.5 text-para2 font-medium text-black dark:text-white">
-                Medium Pack
-              </h4>
-              <p>Lorem ipsum dolor sit amet, consec adipisicing elit.</p>
-
-              <div className="mt-9 border-t border-stroke pb-12.5 pt-9 dark:border-strokedark">
-                <ul>
-                  <li className="mb-4 text-black last:mb-0 dark:text-manatee">300 GB Storage</li>
-                  <li className="mb-4 text-black last:mb-0 dark:text-manatee">
-                    Unlimited Photos and Videos
-                  </li>
-                  <li className="mb-4 text-black last:mb-0 dark:text-manatee">Exclusive Support</li>
-                  <li className="mb-4 text-black opacity-40 last:mb-0 dark:text-manatee">
-                    Custom Branding Strategy
-                  </li>
-                </ul>
-              </div>
-
-              <button
-                aria-label="Get the Plan button"
-                className="group/btn inline-flex items-center gap-2.5 font-medium text-primary transition-all duration-300 dark:text-white dark:hover:text-primary"
-              >
-                <span className="duration-300 group-hover/btn:pr-2">Get the Plan</span>
-                <FaArrowRight className="h-4 w-4" />
-              </button>
-            </div>
-          </div>
+          <PricingTable products={products} action="session" />
         </div>
       </section>
-      {/* <!-- ===== Pricing Table End ===== --> */}
     </>
   );
 };
