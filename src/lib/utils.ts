675fda34fb2dc21ee901192cad469c43a79c3d35
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2024-01-08 13:40:36 +0100

    add a price formatter

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
+import Stripe from "stripe";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -7,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function absoluteUrl(path: string) {
   if (typeof window !== "undefined") return path;
-  if (process.env.NEXT_BASE_URL) return `${process.env.NEXT_BASE_URL}/${path}`;
+  if (process.env.NEXT_PUBLIC_BASE_URL) return `${process.env.NEXT_PUBLIC_BASE_URL}/${path}`;
   else return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
 
@@ -15,4 +16,14 @@ export const toDateTime = (secs: number) => {
   var t = new Date("1970-01-01T00:30:00Z"); // Unix epoch start.
   t.setSeconds(secs);
   return t;
+};
+
+export const formatPrice = (price: number = 0, currency?: Stripe.Price["currency"]) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: currency ?? "USD",
+    minimumFractionDigits: 0,
+  });
+  const formattedPrice = formatter.format(price / 100);
+  return formattedPrice;
 };
